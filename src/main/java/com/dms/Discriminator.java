8fd8a75cab6d475fba5958aae5783db9f00d60b9
dmarius99 <marius.dinu@gmail.com>
2015-01-29 15:18:45 +0200

    use single import statements

--- src/main/java/com/dms/Discriminator.java
+++ src/main/java/com/dms/Discriminator.java
@@ -1,6 +1,11 @@
 package com.dms;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Discriminator, 17.10.2014
